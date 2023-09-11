7f99a65f97781c6f9964aa8b8dfc7df7551c302f
Feyza Nur Bakır <120409251+feyzabakir@users.noreply.github.com>
2023-09-11 17:30:53 +0300

    Category Api

--- RealEstate_Dapper_UI/Controllers/CategoryController.cs
+++ RealEstate_Dapper_UI/Controllers/CategoryController.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RealEstate_Dapper_UI.Dtos.CategoryDtos;
 
 namespace RealEstate_Dapper_UI.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategoryController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44303/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var JsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(JsonData);
+                return View(values);
+            }
             return View();
         }
     }
