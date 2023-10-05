47516f05a34aee4d4b2aa34d5a936cc3521bac40
Feyza Nur Bakır <feyzanurbakirr@gmail.com>
2023-10-05 21:33:13 +0300

    Employee

--- RealEstate_Dapper_Api/Repositories/EmployeeRepositories/EmployeeRepository.cs
+++ RealEstate_Dapper_Api/Repositories/EmployeeRepositories/EmployeeRepository.cs
@@ -14,14 +14,14 @@ namespace RealEstate_Dapper_Api.Repositories.EmployeeRepositories
         }
         public async void CreateEmployee(CreateEmployeeDto createEmployeeDto)
         {
-            string query = "insert into Employee (Name,Title,Mail,PhoneNumber,ImageUrl,Status) values (@name, @title, @mail, @phoneNumber,@imageUrl,@status)";
+            string query = "insert into Employee (Name,Title,Mail,PhoneNumber,ImageUrl,Status) values (@name,@title,@mail,@phoneNumber,@imageUrl,@status)";
             var parameters = new DynamicParameters();
             parameters.Add("@name", createEmployeeDto.Name);
             parameters.Add("@title", createEmployeeDto.Title);
             parameters.Add("@mail", createEmployeeDto.Mail);
             parameters.Add("@phoneNumber", createEmployeeDto.PhoneNumber);
             parameters.Add("@imageUrl", createEmployeeDto.ImageUrl);
-            parameters.Add("@staus", true);
+            parameters.Add("@status", true);
             using (var connection = _context.CreateConnection())
             {
                 await connection.ExecuteAsync(query, parameters);
@@ -51,7 +51,7 @@ namespace RealEstate_Dapper_Api.Repositories.EmployeeRepositories
 
         public async Task<GetByIDEmployeeDto> GetEmployee(int id)
         {
-            string query = "Select * From Employee Where EmployeeID=@employeeID";
+            string query = "Select * From Employee Where EmployeeID=@EmployeeID";
             var parameters = new DynamicParameters();
             parameters.Add("@EmployeeID", id);
             using (var connection = _context.CreateConnection())
@@ -63,15 +63,15 @@ namespace RealEstate_Dapper_Api.Repositories.EmployeeRepositories
 
         public async void UpdateEmployee(UpdateEmployeeDto updateEmployeeDto)
         {
-            string query = "Update Employee Set Name=@name,Title=@title,Mail=@mail,PhoneNumber=@phoneNumber,ImageUrl=@imageUrl,Status=@staus where EmployeeID=@employeeID";
+            string query = "Update Employee Set Name=@name,Title=@title,Mail=@mail,PhoneNumber=@phoneNumber,ImageUrl=@imageUrl,Status=@status where EmployeeID=@employeeId";
             var parameters = new DynamicParameters();
             parameters.Add("@name", updateEmployeeDto.Name);
             parameters.Add("@title", updateEmployeeDto.Title);
             parameters.Add("@mail", updateEmployeeDto.Mail);
             parameters.Add("@phoneNumber", updateEmployeeDto.PhoneNumber);
             parameters.Add("@imageUrl", updateEmployeeDto.ImageUrl);
-            parameters.Add("@staus", true);
-            parameters.Add("@employeeID", updateEmployeeDto.EmployeeID);
+            parameters.Add("@status", updateEmployeeDto.Status);
+            parameters.Add("@employeeId", updateEmployeeDto.EmployeeID);
             using (var connection = _context.CreateConnection())
             {
                 await connection.ExecuteAsync(query, parameters);
